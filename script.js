c0ae60767aa312434706387226de298bda932dfb
urud.theo@gmail.com <urud.theo@gmail.com>
2024-01-17 16:59:27 +0100

    project done, only missing: color change of the bar in form

---
+++ script.js
@@ -0,0 +1,8 @@
+// const emailClicked = document.getElementById("email");
+// const emailContainer = document.querySelectorAll(".email-container");
+// const passwordClicked = document.getElementById("password");
+// const passwordContainer = document.querySelector(".password-container");
+
+// emailClicked.addEventListener("click", () => {
+//   emailContainer.classList.toggle("clicked");
+// });
